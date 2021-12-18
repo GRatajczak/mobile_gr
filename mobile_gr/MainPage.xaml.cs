b7d6e408874c5b80c270136f596eec1fba7c8210
Grzegorz <g.ratajczak97@gmail.com>
2021-12-18 15:19:19 +0100

    changed to xaml

--- mobile_gr/MainPage.xaml.cs
+++ mobile_gr/MainPage.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace mobile_gr
@@ -11,11 +16,55 @@ namespace mobile_gr
 
     public partial class MainPage : ContentPage
     {
+
+        string translatedNumber = default(string);
         public MainPage()
         {
             InitializeComponent();
+          
+        }
+        void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumber.Text;
+            translatedNumber = mobile_gr.PhoneTranslator.ToNumber(enteredNumber);
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                call.IsEnabled = true;
+                call.Text = "Zadźwoń do " + translatedNumber;
+            }
+            else
+            {
+                call.IsEnabled = true;
+                call.Text = "Zadźwoń do " + translatedNumber;
+            }
         }
 
-     
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                   "Wybierz numer",
+                   "Zadzwonić do  " + translatedNumber + "?",
+                   "Tak",
+                   "Nie"))
+            {
+                try
+                {
+                    PhoneDialer.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Nie można zadzwonić", "Numer telefonu jest zły", "OK");
+                }
+                catch (FeatureNotSupportedException)
+                {
+                    await DisplayAlert("Nie można zadzwonić", "Zamiana numeru jest nie wspierana", "OK");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Nie można zadzwonić", "Wystąpił błąd zamiany.", "OK");
+                }
+            }
+
+        }
     }
 }
